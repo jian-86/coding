054d471c7c21ce519bdb2e56d124a421787ac5b7
jian <p.o0x0oa@gmail.com>
2024-05-19 16:06:56 +0900

    고양이

---
+++ java/입출력과 사칙연산/T10171.java
@@ -0,0 +1,20 @@
+
+/**
+ * 고양이
+ * * 문제
+ * Hello World!를 출력하시오.
+ * * 입력
+ * 없음
+ * * 출력
+ * 고양이를 출력한다.
+ */
+public class T10171 {
+    public static void main(String[] args) {
+
+        System.out.println("\\    /\\");
+        System.out.println(" )  ( ')");
+        System.out.println("(  /  )");
+        System.out.println(" \\(__)|");
+    }
+
+}
